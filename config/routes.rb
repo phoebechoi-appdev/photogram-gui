e36c6672a9247bfd003dd6ec2b703f4bc02f0a26
phoebechoi-uc <phoebechoi@uchicago.edu>
2021-11-09 19:50:21 +0000

    done with delete

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   get("/photos", { :controller => "photos", :action => "index" })
   get("/photos/:path_id", { :controller => "photos", :action => "show"})
+  
+  get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii"})
 end
